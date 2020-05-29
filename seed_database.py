4017119c3c586c42f4e166a0d8069521641dbe91
Julie Hu <jhu21495@gmail.com>
2020-05-29 16:24:50 -0700

    loops over movies

---
+++ seed_database.py
@@ -0,0 +1,41 @@
+"""parses through data (movies.json) and automates creation of db"""
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+import crud, model, server
+
+os.system('dropdb ratings')
+os.system('createdb ratings')
+
+# connect to database 
+model.connect_to_db(server.app)
+model.db.create_all()
+
+
+# load movie data from data/movies.json and save to variable
+with open('data/movies.json') as f:
+	movie_data = json.loads(f.read())
+
+#create movies, store them in a list so we can use them
+# to create fake ratings later 
+
+movies_in_db = []
+
+for movie in movie_data:
+	# TODO: get the title, overview, and poster_path from the movie
+    # dictionary. Then, get the release_date and convert it to a
+    # datetime object with datetime.strptime
+
+    # TODO: create a movie here and append it to movies_in_db
+
+    title, overview, poster_path = (movie['title'],
+    								movie['overview'],
+    								movie['poster_path'])
+
+    format = "%Y-%m-%d"
+    release_date = datetime.strptime(movie['release_date'], format)
+
+    db_movie = crud.create_movie(title,overview, release_date, poster_path) 
+
+    movies_in_db.append(db_movie)
